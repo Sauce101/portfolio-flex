78de36899ef7130997c0e24e92fc631e5cde5896
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2023-06-30 23:47:04 -0700

    added AppsIcon removed LogoDevIcon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import Box from '@mui/material/Box'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import LogoDevIcon from '@mui/icons-material/LogoDev'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
 import LightModeIcon from '@mui/icons-material/LightMode'
+import AppsIcon from '@mui/icons-material/Apps'
 import { amber, yellow } from '@mui/material/colors'
 
 const StyledToolbar = styled(Toolbar)({
@@ -30,10 +30,10 @@ const Navbar = ({ duskdawn, daytime, executeScroll }) => {
           onClick={executeScroll}
           sx={{ alignItems: 'flex-end', display: { xs: 'none', sm: 'flex' } }}
         >
-          <LogoDevIcon fontSize="large" sx={{ mr: 1 }} />
+          <AppsIcon fontSize="large" sx={{ mr: 1 }} />
           <Typography variant="h6">Portfolio</Typography>
         </Box>
-        <LogoDevIcon
+        <AppsIcon
           onClick={executeScroll}
           fontSize="large"
           sx={{ display: { xs: 'block', sm: 'none' } }}

90f2e5170e13d22c64eb145a6f90613c1a584a65
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2024-03-18 18:53:18 -0700

    Added persistent dark mode

---
+++ src/hooks/useColorScheme.js
@@ -0,0 +1,32 @@
+import { useMemo } from 'react'
+import { useMediaQuery } from 'react-responsive'
+import createPersistedState from 'use-persisted-state'
+const useColorSchemeState = createPersistedState('colorScheme')
+
+export function useColorScheme() {
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: '(prefers-color-scheme: dark)',
+    },
+    undefined
+  )
+
+  const [daytime, setDaytime] = useColorSchemeState()
+  const value = useMemo(
+    () => (daytime === undefined ? !!systemPrefersDark : daytime),
+    [daytime, systemPrefersDark]
+  )
+
+  // useEffect(() => {
+  //   if (value) {
+  //     document.body.classList.add('dark')
+  //   } else {
+  //     document.body.classList.remove('dark')
+  //   }
+  // }, [value])
+
+  return {
+    daytime: value,
+    setDaytime,
+  }
+}
